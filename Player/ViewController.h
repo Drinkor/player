7f9988bb6a1002d03a44a1ab2307e53ba3db3d9d
Евгений Сергеев <Jenejkee@MacBook-Air-Evgenij.local>
2014-08-21 16:47:19 +0400

    Initial Commit

---
+++ Player/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Player
+//
+//  Created by Евгений Сергеев on 21.08.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
